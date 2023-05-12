444decbf7bcb5834045f437abfefb2fe03f63888
Derrick Allen <derrick0393@gmail.com>
2023-05-11 22:07:05 -0600

    Avance06: Comportamiento del cliente - funcionalidad PopUp del codigo finalizado

--- src/js/codePopUp.js
+++ src/js/codePopUp.js
@@ -1,10 +1,32 @@
-const codePopUp = document.getElementById('popUpCode');
-const cancelButton = document.getElementsByClassName('btnCancel');
 
-function closePopUp () {
-  if(codePopUp && cancelButton) {
-    codePopUp.style.display = 'none';
-  }
+
+const showCodePopUp = () => {
+  let popUp = document.getElementById('popUpCode');
+  popUp.style.display = "flex";
+  verifyCode();
 }
 
-cancelButton[0].addEventListener('click', closePopUp);
+const verifyCode = () => {
+  let codeInput = document.getElementById('popUpInput');
+  codeInput.addEventListener('keydown', (event) => {
+    let key = event.key;
+    let msg = document.getElementById("msgCode");
+    if (isNaN(key)){
+      msg.innerHTML  = "Sala no existe";
+    } else {
+      msg.innerHTML  = "Sala encontrada";
+    }
+  });
+}
+
+const cancelPopUp = () => {
+  let popUp = document.getElementById('popUpCode');
+  popUp.style.display = "none";
+  document.getElementById('popUpInput').value = '';
+  document.getElementById("msgCode").innerHTML = '';
+}
+
+const joinRoom = () => {
+  // code validation
+  location.href = './waitingRoom.xhtml';
+}
--- src/js/homePage.js
+++ src/js/homePage.js
@@ -1,4 +1,4 @@
 function showCodePopUp () {
     let popUp = document.getElementById("codePopUp");
-    popUp.style.visibility = "visible";
+    popUp.style.display = "flex"
 }
