e4767f36031f44399e05aab20ac7dbc237609dd9
ximenaGdur <68168666+ximenaGdur@users.noreply.github.com>
2023-05-14 15:59:15 -0600

    Avance06: Comportamiento del lado del cliente
    Avances con common css

--- src/js/codePopUp.js
+++ src/js/codePopUp.js
@@ -1,10 +1,21 @@
+/******************** Creating constants for script ********************/
+
+// Pop Up that is shown when player decides to join session.
 const codePopUp = document.getElementById('popUpCode');
+// Button that allows player to close pop up.
 const cancelButton = document.getElementById('btnCancel');
 
+/********************** Functions used on script **********************/
+
+/**
+ * Close Pop Up.
+ */
 function closePopUp () {
   if(codePopUp && cancelButton) {
     codePopUp.style.display = 'none';
   }
 }
+
+/************************ Listeners for buttons ************************/
 
 cancelButton.addEventListener('click', closePopUp);
