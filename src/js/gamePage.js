cbc3f5622bdc29ca79f34f78181de1f7db803812
Gianf1612 <gianf1612@gmail.com>
2023-05-18 01:10:49 -0600

    Deleted comments

--- src/js/gamePage.js
+++ src/js/gamePage.js
@@ -5,6 +5,9 @@
 /******************** Creating constants for script ********************/
 const gameBoardImages = document.getElementsByClassName('board-image');
 const exitButton = document.getElementById('exit-button');
+const myImage = document.getElementsByClassName('my-image');
+let word = document.getElementsByClassName('word');
+let myImages = document.getElementsByClassName('my-image-container');
 let boardImages = document.getElementsByClassName('board-image-container');
 let blurTime = null;
 const maxTime = 5000;
@@ -34,33 +37,22 @@ function loadPage() {
  * Changes images in "myFichas" to words
 */
 
-function changeImagesToWords() {
-    // Obtener todas las imágenes de la página
-    const myImage = document.getElementsByClassName('my-image');
-    let word = document.getElementsByClassName('word');
-    let myImages = document.getElementsByClassName('my-image-container');
-    
-    // Iterar a través de cada imagen y reemplazar su contenido por el atributo "alt"
-    for(let index = 0; index < myImage.length; index ++){
+function changeImagesToWords() {    
+    // Iterate through each image and replace its content with the attribute "alt"
+    for(let index = 0; index < myImage.length; index ++) {
         myImage[index].style.display='none';
         word[index].style.display='flex';
         // changes box to fit words 
         myImages[index].style.display.alingSelf='stretch'
         myImages[index].style.maxWidth = 'max-content';
-        
     }
-    //if( myImage.style.display != "none")
-    //my-image-container{
-    //    align-self: stretch;
-    //    max-width: max-content;
-    //    .my-image{
-    //        display: none;
 }
 
 /************************ TIME FUNCTIONS ************************/
 
 /** handleMaxTime 
- * when changing max time, update **/
+ * when changing max time, update 
+ */
 function updateTime(time) {
     var timeLeft = time;
     var timer = "Tiempo";
@@ -70,7 +62,8 @@ function updateTime(time) {
 }
 
 /** handleTimesUp
- * if time runs out the game is over **/
+ * if time runs out the game is over 
+ */
 function TimesUp(time) {
     if( time == 0) {
         popUpFinished.style.display = 'flex';
@@ -81,7 +74,7 @@ function TimesUp(time) {
 /**
  * 
  */
-function changeImageColors(){
+function changeImageColors() {
     for(let index = 0; index < boardImages.length; index ++) {
         boardImages[index].style.borderColor = randomBorderColor();
     }
@@ -91,7 +84,7 @@ function changeImageColors(){
  * 
  * @returns 
  */
-function randomBorderColor(){
+function randomBorderColor() {
     // Código tomado de: https://www.delftstack.com/es/howto/javascript/javascript-pick-random-from-array/
     var colorsArray = ['#E6C700', '#2EB600', '#006DE2', '#DA0012']
     var randomIndex = Math.floor(Math.random()*colorsArray.length);
